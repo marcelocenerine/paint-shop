394b71191b68617f7ff9812caa378f0c6095c86d
Marcelo Cenerino <marcelocenerine@gmail.com>
2017-09-17 20:30:18 +0100

    implement optmizations to reduce search space

--- src/main/scala/paintshop/Mixer.scala
+++ src/main/scala/paintshop/Mixer.scala
@@ -1,26 +1,75 @@
 package paintshop
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object Mixer {
 
   def mix(selections: List[PaintSelection]): Option[PaintSelection] = {
     if (selections.isEmpty) None
     else {
-      val distinctColors = (for ( s <- selections; p <- s.paints) yield p.color).toSet
-
-      if (distinctColors.isEmpty) Some(PaintSelection(Set.empty))
-      else {
-        def satisfiesAll(mix: Set[Paint]): Boolean = {
-          def isHappy(asked: PaintSelection) = asked.paints.exists(mix.contains(_))
-
-          selections.forall(isHappy)
-        }
-
-        val solutions = findFeasibleSolutions(distinctColors, Sheen.all, satisfiesAll)
-        solutions.sortBy(sel => sel.paints.foldRight(0)((p, acc) => acc + p.sheen.cost)).headOption
+      reduceSearchSpace(selections.toSet) match {
+        case Some((mustHavePaints, reducedSelections)) =>
+          if (reducedSelections.isEmpty) Some(PaintSelection(mustHavePaints))
+          else {
+            exploreSearchSpace(reducedSelections).map { mix =>
+              PaintSelection(mustHavePaints ++ mix.paints)
+            }
+          }
+          
+        case None => None
       }
     }
+  }
+
+  private def reduceSearchSpace(selections: Set[PaintSelection]): Option[(Set[Paint], Set[PaintSelection])] = {
+    implicit val ordering = Ordering.by[PaintSelection, Int](_.paints.size).reverse // small selections at the top
+    var selectionQueue = mutable.PriorityQueue.empty[PaintSelection] ++ selections
+    var singletonFound = true
+    var deadEndFound = false
+    val mustHavePaints = mutable.Set.empty[Paint]
+
+    while (!selectionQueue.isEmpty && singletonFound && !deadEndFound) {
+      val smallest = selectionQueue.dequeue()
+      val paints = smallest.paints
+
+      if (paints.isEmpty) {
+        // throw away
+      } else if (paints.size == 1) {
+        val singleton = paints.head
+        mustHavePaints += singleton
+        val updated = mutable.PriorityQueue.empty[PaintSelection]
+
+        // remove must-have paint from remaining selections
+        while (!selectionQueue.isEmpty && !deadEndFound) {
+          val ps = selectionQueue.dequeue().paints
+
+          if (ps.size == 1 && ps.head.color == singleton.color && ps.head.sheen != singleton.sheen) {
+            deadEndFound = true // incompatible with other singleton
+          } else {
+            updated += PaintSelection(ps.filterNot(_.color == singleton.color))
+          }
+        }
+        selectionQueue = updated
+      } else {
+        selectionQueue += smallest
+        singletonFound = false
+      }
+    }
+
+    if (deadEndFound) None else Some(mustHavePaints.toSet -> selectionQueue.toSet)
+  }
+
+  private def exploreSearchSpace(selections: Set[PaintSelection]): Option[PaintSelection] = {
+    def satisfiesAll(mix: Set[Paint]): Boolean = {
+      def isHappy(asked: PaintSelection) = asked.paints.exists(mix.contains(_))
+
+      selections.forall(isHappy)
+    }
+
+    val distinctColors = for ( s <- selections; p <- s.paints) yield p.color
+    val solutions = findFeasibleSolutions(distinctColors, Sheen.all, satisfiesAll)
+    solutions.sortBy(s => s.paints.foldRight(0)((p, totalCost) => totalCost + p.sheen.cost)).headOption
   }
 
   /**
